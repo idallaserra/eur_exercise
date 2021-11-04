e6556971b2cb1fb2a7653006f3ed58ed176f24c6
idallaserra <ivan.dallaserra@gmail.com>
2021-11-04 22:08:35 +0100

    removed unused printf in app

--- app/app.py
+++ app/app.py
@@ -35,36 +35,9 @@ def home():
 
 @app.route("/hostinfo")
 def hostinfo():
-    # let's print CPU information
-    print("="*40, "CPU Info", "="*40)
-    # number of cores
-    print("Physical cores:", psutil.cpu_count(logical=False))
-    print("Total cores:", psutil.cpu_count(logical=True))
-    # CPU frequencies
-    cpufreq = psutil.cpu_freq()
-    print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
-    print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
-    print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
-    # CPU usage
-    print("CPU Usage Per Core:")
-    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
-        print(f"Core {i}: {percentage}%")
-    #return f"Total CPU Usage: {psutil.cpu_percent()}% \n Test"
-    print("="*40, "Memory Information", "="*40)
     # get the memory details
     svmem = psutil.virtual_memory()
-    print(f"Total: {get_size(svmem.total)}")
-    print(f"Available: {get_size(svmem.available)}")
-    print(f"Used: {get_size(svmem.used)}")
-    print(f"Percentage: {svmem.percent}%") 
-    print("="*20, "SWAP", "="*20)
-    # get the swap memory details (if exists)
-    #swap = psutil.swap_memory()
-    #print(f"Total: {get_size(swap.total)}")
-    #print(f"Free: {get_size(swap.free)}")
-    #print(f"Used: {get_size(swap.used)}")
-    #print(f"Percentage: {swap.percent}%")
-    print("="*40, "System Information", "="*40)
+    # get the platform details
     uname = platform.uname()
     return f"Total CPU Usage: {psutil.cpu_percent()}%, Ram: Total - {get_size(svmem.total)}, Available - {get_size(svmem.available)}, Used - {get_size(svmem.used)}, Percentage - {svmem.percent}%, System: {uname.system}, Node Name: {uname.node},  Release: {uname.release}, Version: {uname.version}, Machine: {uname.machine}, Processor: {uname.processor}"
 
